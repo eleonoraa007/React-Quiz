2e04dab551849609aa653be7b5b76d2c92d3f2c2
eleonoraa007 <hrubenja.eleonora@gmail.com>
2024-02-05 13:00:32 +0100

    finished project

---
+++ src/components/Timer.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+
+      //instead of creating a new timer each time you restart a quiz
+      //we repeat the timer using clearInterval (clear timer)
+      return () => clearInterval(id);
+    },
+    [dispatch]
+  );
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
+}
+
+export default Timer;
